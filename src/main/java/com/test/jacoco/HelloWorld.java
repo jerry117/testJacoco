abb63d581e25cb0af6425c46efcf2ef565f0861a
lijialiang <lijialiang@banggood.com>
2019-01-03 10:01:27 +0800

    jacocodemo

---
+++ src/main/java/com/test/jacoco/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.test.jacoco;
+
+public class HelloWorld {
+    public HelloWorld(){
+
+    }
+    public String testMethod1(){
+        return "hello world!";
+    }
+
+    public int addMethod(int a,int b){
+        return a+b;
+    }
+}
+
---
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -0,0 +1,27 @@
+package com.test.jacoco.test;
+
+
+import com.test.jacoco.HelloWorld;
+import org.junit.Test;
+import junit.framework.*;
+
+public class HelloWorldTest {
+
+//TODO 后续看注解的定义
+//    TODO 后续补充maven的概念和方法
+
+    @Test
+    public void testMethod1(){
+        HelloWorld hw = new HelloWorld();
+        String ss = hw.testMethod1();
+        Assert.assertNotNull(ss);
+    }
+
+    @Test
+    public void testMethod2(){
+        HelloWorld hw = new HelloWorld();
+        int ss = hw.addMethod(3,5);
+        Assert.assertEquals(ss,8);
+    }
+
+}
